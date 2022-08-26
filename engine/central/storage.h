951275fc3def4a673959454ea29e3baff79baef1
lukas <lukas.niegsch@stud.uni-goettingen.de>
2022-08-26 23:06:10 +0200

    added storage class for ECS system

---
+++ engine/central/storage.h
@@ -0,0 +1,81 @@
+#pragma once
+#include <any>
+#include <map>
+#include <set>
+#include <vector>
+#include <unordered_map>
+#include <typeindex>
+#include <memory>
+
+namespace Kodanuki {
+
+/**
+ * The entity storage class contains arbitrary amount of data.
+ * 
+ * Most methods use sparse identifiers that map to positions inside the dense
+ * vector. Values are stored using std::any to avoid templates. However, all
+ * elements should be of the same type.
+ */
+class EntityStorage
+{
+public:
+	// Updates the given element or inserts it into the storage.
+	void update(int identifier, std::any value);
+
+	// Removes the given element from the storage.
+	void remove(int identifier);
+
+	// Returns the reference to the element.
+	std::any& get(int identifier);
+
+	// Returns true iff the element is inside the storage.
+	bool contains(int identifier) const;
+
+	// Returns the number of dense elements inside the storage.
+	int size() const;
+
+	// Returns all sparse identifiers inside the storage.
+	std::set<int> identifiers(bool includeWeakBindings = false);
+
+	// Copies the source element to the target element inside the storage.
+	void copy(int sourceIdentifier, int targetIdentifier);
+
+	// Moves the source element to the target element inside the storage.
+	void move(int sourceIdentifier, int targetIdentifier);
+
+	// Swaps the source element with the target element inside the storage.
+	void swap(int sourceIdentifier, int targetIdentifier);
+
+	// Binds the source element to the target element inside the storage.
+	void bind(int sourceIdentifier, int targetIdentifier, bool weak = false);
+
+private:
+	using EID = int; // entity identifier
+	using SID = int; // storage identifier
+	using POS = int; // dense position
+	std::map<EID, SID> bindings;
+	std::set<EID> weakBindings;
+	std::map<SID, POS> sparse;
+	std::map<POS, SID> sparseInverse;
+	std::map<SID, int> sparseCount;
+	std::vector<std::any> dense;
+};
+
+using Storage = std::unique_ptr<EntityStorage>;
+using Mapping = std::unordered_map<std::type_index, Storage>;
+
+/**
+ * Returns the storage for the given mapping. Missing type entries will be
+ * added automatically.
+ */
+template <typename T>
+static EntityStorage* getStorage(Mapping& mapping)
+{
+	auto type = std::type_index(typeid(T));
+	if (mapping.count(type) == 0) {
+		mapping[type] = std::make_unique<EntityStorage>();
+	}
+	return mapping[type].get();
+}
+
+} // namespace Kodanuki
---
+++ unittest/central/storage-test.cpp
@@ -0,0 +1,169 @@
+#include "engine/central/storage.h"
+#include <doctest/doctest.h>
+using namespace Kodanuki;
+
+TEST_CASE("entity storage tests")
+{
+    EntityStorage storage;
+
+	SUBCASE("new storage is empty")
+	{
+		CHECK(storage.size() == 0);
+	}
+
+	SUBCASE("new elements increase the size")
+	{
+		storage.update(0, 42);
+		CHECK(storage.size() == 1);
+		storage.update(1, 69);
+		CHECK(storage.size() == 2);
+	}
+
+	SUBCASE("elements with same identifier don't increase the size")
+	{
+		storage.update(0, 42);
+		storage.update(0, 69);
+		CHECK(storage.size() == 1);
+	}
+
+	SUBCASE("storage contains new element")
+	{
+		storage.update(0, 42);
+		CHECK(storage.contains(0));
+	}
+
+	SUBCASE("storage does not contain element after removing it")
+	{
+		storage.update(0, 42);
+		storage.remove(0);
+		CHECK(storage.contains(0) == false);
+		CHECK(storage.size() == 0);
+	}
+
+	SUBCASE("storage stores identifiers")
+	{
+		storage.update(0, 42);
+		storage.update(1, 69);
+		std::set identifiers = storage.identifiers();
+		CHECK(identifiers.count(0) == true);
+		CHECK(identifiers.count(1) == true);
+	}
+
+	SUBCASE("storage identifiers exclude weak bindings by default")
+	{
+		storage.update(0, 42);
+		storage.bind(1, 0, true);
+		std::set identifiers = storage.identifiers();
+		CHECK(identifiers.count(0) == true);
+		CHECK(identifiers.count(1) == false);
+	}
+
+	SUBCASE("storage identifiers can include weak bindings")
+	{
+		storage.update(0, 42);
+		storage.bind(1, 0, true);
+		std::set identifiers = storage.identifiers(true);
+		CHECK(identifiers.count(0) == true);
+		CHECK(identifiers.count(1) == true);
+	}
+
+	SUBCASE("storage saves element data")
+	{
+		storage.update(0, 42);
+		storage.update(1, 69);
+		CHECK(std::any_cast<int>(storage.get(0)) == 42);
+		CHECK(std::any_cast<int>(storage.get(1)) == 69);
+	}
+
+	SUBCASE("storage can copy elements")
+	{
+		storage.update(0, 42);
+		storage.copy(0, 1);
+		CHECK(storage.contains(0) == true);
+		CHECK(storage.contains(1) == true);
+		CHECK(std::any_cast<int>(storage.get(0)) == 42);
+		CHECK(std::any_cast<int>(storage.get(1)) == 42);
+	}
+
+	SUBCASE("updating the same element overwrites it")
+	{
+		storage.update(0, 42);
+		CHECK(std::any_cast<int>(storage.get(0)) == 42);
+		storage.update(0, 69);
+		CHECK(std::any_cast<int>(storage.get(0)) == 69);
+	}
+
+	SUBCASE("storage can move elements")
+	{
+		storage.update(0, 42);
+		storage.move(0, 1);
+		CHECK(storage.contains(0) == false);
+		CHECK(storage.contains(1) == true);
+		CHECK(std::any_cast<int>(storage.get(1)) == 42);
+	}
+
+	SUBCASE("storage can swap elements")
+	{
+		storage.update(0, 42);
+		storage.update(1, 69);
+		storage.swap(0, 1);
+		CHECK(storage.contains(0) == true);
+		CHECK(storage.contains(1) == true);
+		CHECK(std::any_cast<int>(storage.get(0)) == 69);
+		CHECK(std::any_cast<int>(storage.get(1)) == 42);
+	}
+
+	SUBCASE("storage can bind elements")
+	{
+		storage.update(1, 42);
+		storage.bind(0, 1);
+		CHECK(storage.contains(0) == true);
+		CHECK(storage.contains(1) == true);
+		CHECK(std::any_cast<int>(storage.get(0)) == 42);
+		CHECK(std::any_cast<int>(storage.get(1)) == 42);
+		storage.update(1, 69);
+		CHECK(storage.contains(0) == true);
+		CHECK(std::any_cast<int>(storage.get(0)) == 69);
+		storage.remove(1);
+		CHECK(storage.contains(0) == true);
+		CHECK(storage.contains(1) == false);
+		CHECK(std::any_cast<int>(storage.get(0)) == 69);
+	}
+
+	SUBCASE("size includes weak bindings")
+	{
+		storage.update(1, 42);
+		storage.bind(0, 1, true);
+		CHECK(storage.size() == 2);
+	}
+
+	SUBCASE("stress testing")
+	{
+		const int count = 100000;
+		for (int i = 0; i < count; i++)
+		{
+			storage.update(i, i);
+		}
+		for (int i = count / 3; i <= 2 * count / 3; i++)
+		{
+			storage.remove(i);
+		}
+
+		bool isSparseWorking = true;
+		bool isDenseWorking = true;
+		for (int i = 0; i < count; i++)
+		{
+			if (count / 3 <= i && i <= 2 * count / 3)
+			{
+				isSparseWorking &= !storage.contains(i);
+			}
+			else
+			{
+				isSparseWorking &= storage.contains(i);
+				isDenseWorking &= std::any_cast<int>(storage.get(i)) == i;
+			}
+		}
+		CHECK(isSparseWorking);
+		CHECK(isDenseWorking);
+	}
+}
