0d21ec37d021e00c45b585378e9cfc537822f2f0
lukas <lukas.niegsch@stud.uni-goettingen.de>
2022-09-07 22:04:42 +0200

    added playground for testing custom code

--- unittest/main.cpp
+++ unittest/main.cpp
@@ -1,2 +1,10 @@
+#include "engine/central/entity.h"
+#include "engine/central/archetype.h"
 #define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
 #include <doctest/doctest.h>
+using namespace Kodanuki;
+
+TEST_CASE("playground")
+{
+
+}
