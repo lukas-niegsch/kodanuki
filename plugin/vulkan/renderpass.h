8f7921fa703add2236d89ebd5303f6694f587cec
lukas.niegsch <lukas.niegsch@stud.uni-goettingen.de>
2022-10-18 16:24:47 +0200

    added some documentation

--- plugin/vulkan/renderpass.h
+++ plugin/vulkan/renderpass.h
@@ -8,23 +8,50 @@
 namespace Kodanuki
 {
 
+/**
+ * Contains all the configurable information for creating a renderpass.
+ *
+ * These values will be considered as much as possible when creating
+ * the renderpass. The caller has to make sure that the provided values
+ * make sense.
+ *
+ * Example:
+ * The device must match the device of the pipeline.
+ * The attachments must match the shaders.
+ */
 struct RenderpassBuilder
 {
+	// The vulkan device for which the renderpass is created.
 	VulkanDevice device;
+
+	// The list of subpass dependencies.
 	std::vector<VkSubpassDependency> dependencies;
+
+	// The list of the attachment descriptions.
 	std::vector<VkAttachmentDescription> attachments;
+
+	// The list of the render subpasses.
 	std::vector<VkSubpassDescription> subpasses;
 };
 
+/**
+ * The vulkan renderer is a wrapper around the renderpass.
+ * 
+ * Instances can be copied around freely and will release all
+ * ressources once unused.
+ */
 class VulkanRenderpass
 {
 public:
+	// Creates a new vulkan rendererpass from the given builder.
 	VulkanRenderpass(RenderpassBuilder builder);
 
 public:
+	// Returns the handle to the actual renderpass.
 	VkRenderPass renderpass();
 
 private:
+	// Destroys unused rendererpasses automatically.
 	std::shared_ptr<Entity> pimpl;
 };
 
