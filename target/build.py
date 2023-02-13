da7c56fa872a9770330f0b924cfb627b935c70d7
lukas.niegsch <lukas.niegsch@stud.uni-goettingen.de>
2023-02-13 19:15:29 +0100

    moved ninja_writer outside of build script

--- target/build.py
+++ target/build.py
@@ -5,6 +5,7 @@ import pathlib
 import tomli
 import subprocess
 import os
+from ninja_writer import NinjaWriter
 
 FLAGS = "-O2 -std=c++2b -Wall -Wextra -Werror -g"
 PROJECTS = "unittest kodanuki tetris splash"
@@ -18,36 +19,6 @@ def parse_arguments():
 	parser.add_argument('project', help = 'The current runnable project.')
 	parser.add_argument('target', help = 'The build target for this script.')
 	return parser.parse_args()
-
-
-class NinjaWriter:
-	def __init__(self):
-		self.lines = []
-
-	def write(self, filename):
-		with open(filename, 'w') as ninja_file:
-			ninja_file.write('\n'.join(self.lines))
-			ninja_file.write('\n')
-
-	def newline(self):
-		self.lines.append('')
-
-	def variable(self, key, value, indent = 0, use_equals = True):
-		prefix = '  ' * indent
-		equals = ' = ' if use_equals else ' '
-		self.lines.append(f'{prefix}{key}{equals}{value}')
-
-	def rule(self, name, command, variables = {}):
-		self.variable('rule', name, use_equals = False)
-		self.variable('command', command, indent = 1)
-		for key, value in variables.items():
-			self.variable(key, value, indent = 1)
-		self.newline()
-
-	def build(self, input, output, variables = {}):
-		self.lines.append(f'build {output}: {input}')
-		for key, value in variables.items():
-			self.variable(key, value, indent = 1)
 
 
 @dataclasses.dataclass
