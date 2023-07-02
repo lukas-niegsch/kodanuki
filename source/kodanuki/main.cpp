aa60f130968569459926fc610502dca855aecaaa
lukas.niegsch <lukas.niegsch@stud.uni-goettingen.de>
2023-07-02 20:57:08 +0200

    added test code for window

--- source/kodanuki/main.cpp
+++ source/kodanuki/main.cpp
@@ -1,6 +1,18 @@
 #include <iostream>
+#include "engine/display/window.h"
+using namespace kodanuki;
 
 int main()
 {
-	std::cout << "Hello, world!" << '\n';
+	Window window = Window::create({
+		.owner  = std::nullopt,
+		.dtype  = Window::Vulkan | Window::OpenGL,
+		.title  = "Kodanuki",
+		.icon   = "assets/images/tanuki.png",
+		.flags  = Window::Resizable | Window::Decorated,
+		.shape  = {600, 600},
+		.frames = 60
+	});
+
+	while (window.tick()) {}
 }
