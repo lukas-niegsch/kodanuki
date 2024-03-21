64ad6832c9159830c51980df4bf82005af623dc1
lukas.niegsch <lukas.niegsch@stud.uni-goettingen.de>
2024-03-21 20:14:55 +0100

    removed old splash project

--- source/splash/user_interface_backend.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-#define IMGUI_DEFINE_MATH_OPERATORS
-#include "extern/imgui/imgui.h"
-#include "extern/imgui/backends/imgui_impl_vulkan.h"
-#include "extern/imgui/backends/imgui_impl_glfw.h"
